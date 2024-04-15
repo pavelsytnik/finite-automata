8b8214f115a1b22ed362158e9d256d9b69181b1b
Pavel Sytnik <pashasytnik@gmail.com>
2024-04-15 22:14:55 +0400

    Add fields in Transition

--- src/pavelsytnik/automata/Transition.java
+++ src/pavelsytnik/automata/Transition.java
@@ -1,4 +1,14 @@
 package pavelsytnik.automata;
 
 public class Transition {
+
+    private final Symbol currentState;
+    private final Symbol inputSymbol;
+    private final Symbol nextState;
+
+    public Transition(Symbol current, Symbol input, Symbol next) {
+        currentState = current;
+        inputSymbol = input;
+        nextState = next;
+    }
 }
