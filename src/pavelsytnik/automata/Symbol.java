9c12f72c5862a67921617706fe8b8d58476899be
Pavel Sytnik <pashasytnik@gmail.com>
2024-04-15 22:00:09 +0400

    Implement Symbol

--- src/pavelsytnik/automata/Symbol.java
+++ src/pavelsytnik/automata/Symbol.java
@@ -1,4 +1,33 @@
 package pavelsytnik.automata;
 
 public class Symbol {
+
+    private final String sym;
+
+    public Symbol(String sym) {
+        this.sym = sym;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (obj == this)
+            return true;
+        if (obj == null || obj.getClass() != this.getClass())
+            return false;
+
+        Symbol symbol = (Symbol) obj;
+        return symbol.sym.equals(this.sym);
+    }
+
+    @Override
+    public int hashCode() {
+        int result = 17;
+        result = 31 * result + sym.hashCode();
+        return result;
+    }
+
+    @Override
+    public String toString() {
+        return sym;
+    }
 }
