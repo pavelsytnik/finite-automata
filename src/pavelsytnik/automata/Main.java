cc5b2cb0af0a74d188ddc2c8f20e254ffa876d23
Pavel Sytnik <pashasytnik@gmail.com>
2024-04-16 19:14:59 +0400

    Add usage example

---
+++ src/pavelsytnik/automata/Main.java
@@ -0,0 +1,46 @@
+package pavelsytnik.automata;
+
+import java.util.Set;
+import java.util.HashSet;
+
+public class Main {
+    public static void main(String[] args) {
+
+        Set<Symbol> states = new HashSet<>();
+        states.add(new Symbol("S"));
+        states.add(new Symbol("B"));
+        states.add(new Symbol("D"));
+        states.add(new Symbol("E"));
+        states.add(new Symbol("F"));
+        states.add(new Symbol("G"));
+        states.add(new Symbol("H"));
+        states.add(new Symbol("J"));
+        states.add(new Symbol("qf"));
+
+        Set<Symbol> alphabet = new HashSet<>();
+        alphabet.add(new Symbol("0"));
+        alphabet.add(new Symbol("1"));
+        alphabet.add(new Symbol("2"));
+        alphabet.add(new Symbol(","));
+        alphabet.add(new Symbol("+"));
+
+        var start = new Symbol("S");
+
+        Set<Symbol> accept = new HashSet<>();
+        accept.add(new Symbol("qf"));
+
+        var automaton = new Automaton(states, alphabet, start, accept);
+        automaton.addTransition(new Symbol("S"), new Symbol("0"), new Symbol("B"));
+        automaton.addTransition(new Symbol("B"), new Symbol("0"), new Symbol("B"));
+        automaton.addTransition(new Symbol("B"), new Symbol("+"), new Symbol("D"));
+        automaton.addTransition(new Symbol("D"), Symbol.EPSILON, new Symbol("E"));
+        automaton.addTransition(new Symbol("E"), new Symbol("0"), new Symbol("F"));
+        automaton.addTransition(new Symbol("F"), new Symbol(","), new Symbol("G"));
+        automaton.addTransition(new Symbol("G"), new Symbol("1"), new Symbol("H"));
+        automaton.addTransition(new Symbol("H"), new Symbol(","), new Symbol("J"));
+        automaton.addTransition(new Symbol("J"), new Symbol("2"), new Symbol("E"));
+        automaton.addTransition(new Symbol("J"), new Symbol("2"), new Symbol("qf"));
+
+        var dfa = automaton.toDeterministic();
+    }
+}
